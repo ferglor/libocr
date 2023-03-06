098f197f40bbbe06d8af664a9f3c539882045fe2
Fergal Gribben <fergal.gribben@smartcontract.com>
2023-03-06 22:17:15 +0000

    Add delays before send

--- networking/ocr_endpoint_v2.go
+++ networking/ocr_endpoint_v2.go
@@ -3,7 +3,9 @@ package networking
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"sync"
+	"time"
 
 	"go.uber.org/multierr"
 
@@ -289,6 +291,10 @@ func (o *ocrEndpointV2) SendTo(payload []byte, to commontypes.OracleID) {
 		o.sendToSelf(payload)
 		return
 	}
+
+	delay := rand.Intn(500)
+	o.logger.Info("Sleeping before send", commontypes.LogFields{"delay ms": delay})
+	time.Sleep(time.Duration(delay) * time.Millisecond)
 
 	o.streams[to].SendMessage(payload)
 }
